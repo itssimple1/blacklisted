fd1d548c1fe092f54a7b11f95f977229bd39e914
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-13 13:38:35 +0530

    Update and rename blacklist.py to config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+from os import environ
+
+
+API_ID = environ.get("API_ID", 0)
+API_HASH = environ.get("API_HASH", None)
+BOT_TOKEN = environ.get("BOT_TOKEN", None)
+DB_URI = environ.get("DATABASE_URL", None)
