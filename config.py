f3c65b59ec3c88aabae510db3d8990167efd725e
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-09-17 15:07:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import environ
 
 
-def sudo_users(user: str):
+def sudo_users(user):
     users = []
     _sudo_ = user
     if _sudo_:
@@ -16,5 +16,5 @@ API_ID = environ.get("API_ID", 0)
 API_HASH = environ.get("API_HASH", None)
 BOT_TOKEN = environ.get("BOT_TOKEN", None)
 DB_URI = environ.get("DATABASE_URL", None)
-user = environ.get("SUDO_USERS", " ")
+user = environ.get("SUDO_USERS", False)
 SUDO_USERS = sudo_users(user)
